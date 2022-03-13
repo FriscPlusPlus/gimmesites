0f30120f0d3ee938135e1d6819ed8d11d39e1c81
Firas Jelassi <f.jelassi@bsc-solutions.com>
2022-03-13 17:59:46 +0100

    changed name from ipthost to gimmesites

--- index.js
+++ index.js
@@ -3,19 +3,18 @@ const Search = require("./src/BingSearch");
 const chalk = require("chalk");
 const cliSpinners = require("cli-spinners");
 const ora = require("ora");
-
+console.clear(); // clear console, for a better view
 /**
  * LOGO FIRAS JELASSI
  */
 
 console.log(
-  chalk.hex("#FF003C")(`
-    ██╗██████╗ ████████╗██╗  ██╗ ██████╗ ███████╗████████╗
-    ██║██╔══██╗╚══██╔══╝██║  ██║██╔═══██╗██╔════╝╚══██╔══╝
-    ██║██████╔╝   ██║   ███████║██║   ██║███████╗   ██║   
-    ██║██╔═══╝    ██║   ██╔══██║██║   ██║╚════██║   ██║   
-    ██║██║        ██║   ██║  ██║╚██████╔╝███████║   ██║   
-    ╚═╝╚═╝        ╚═╝   ╚═╝  ╚═╝ ╚═════╝ ╚══════╝   ╚═╝  
+  chalk.hex("#FF003C").bgHex("#554B41")(`
+   ██████  ██ ███    ███ ███    ███ ███████ ███████ ██ ████████ ███████ ███████ 
+  ██       ██ ████  ████ ████  ████ ██      ██      ██    ██    ██      ██      
+  ██   ███ ██ ██ ████ ██ ██ ████ ██ █████   ███████ ██    ██    █████   ███████ 
+  ██    ██ ██ ██  ██  ██ ██  ██  ██ ██           ██ ██    ██    ██           ██ 
+   ██████  ██ ██      ██ ██      ██ ███████ ███████ ██    ██    ███████ ███████
     By Firas Jelassi (https://github.com/Friscas/)
 `)
 );
@@ -27,26 +26,29 @@ const BingSearch = new Search("176.52.245.147", {
   bProxy: false,
 });
 
-const SearchSpinner = ora(chalk.hex("#FF003C")("Searching on Bing"));
+const SearchSpinner = ora(chalk.hex("#FF003C").bold("Searching on Bing"));
 const CleanSpinner = ora(
-  chalk.hex("#FF003C")(
-    "Removing duplicates and urls that are hosted on a different server"
-  )
+  chalk
+    .hex("#FF003C")
+    .bold("Removing duplicates and urls that are hosted on a different server")
 );
 
 BingSearch.on("search", () => {
   SearchSpinner.start();
 });
 BingSearch.on("clean", (urlsTotalCount) => {
-  SearchSpinner.succeed(chalk.hex("#FF003C")(`${urlsTotalCount} links found`));
+  SearchSpinner.succeed(
+    chalk.hex("#FF003C").bold(`${urlsTotalCount} links found`)
+  );
   CleanSpinner.start();
 });
 BingSearch.on("links", (url) => {
   if (!cleanDone) {
     cleanDone = true;
     CleanSpinner.succeed();
+    console.log(""); // adding spacing
   }
-  console.log(chalk.hex("#FF003C")(url));
+  console.log(chalk.hex("#FF003C").bold(url));
 });
 
 BingSearch.search();
