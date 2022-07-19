e0bac6f8d0c5461fd2b43470a2fc4bd5ffe497ea
Firas Jelassi <49401280+Friscas@users.noreply.github.com>
2022-07-19 18:46:14 +0200

    added validation for count

--- src/argsHelper.js
+++ src/argsHelper.js
@@ -1,6 +1,6 @@
-const chalk = require("chalk");
-const axios = require("axios").default;
-const { SocksProxyAgent } = require("socks-proxy-agent");
+const chalk = require('chalk');
+const axios = require('axios').default;
+const { SocksProxyAgent } = require('socks-proxy-agent');
 
 /**
  * @Type: Class
@@ -12,51 +12,51 @@ class ArgsHelper {
   noArgs() {
     console.log(
       chalk
-        .hex("#FF003C")
+        .hex('#FF003C')
         .bold('Example: gimmesites --target="185.77.32.227" --count=50')
     );
     console.log(
-      chalk.hex("#FF003C").bold("Call gimmesites --help for more information!")
+      chalk.hex('#FF003C').bold('Call gimmesites --help for more information!')
     );
   }
   error(flag) {
     console.log(
       chalk
-        .hex("#FF003C")
+        .hex('#FF003C')
         .bold(
           `The ${flag} option is incorrectly written or it does not exist, please use the --help flag for more information`
         )
     );
   }
   help() {
-    console.log(chalk.hex("#FF003C").bold(`Options:`));
-    console.log(chalk.hex("#FF003C")(`  -h, --help`));
-    console.log(chalk.hex("#FF003C")(`      display help information`));
-    console.log(chalk.hex("#FF003C")(`  -t, --target`));
+    console.log(chalk.hex('#FF003C').bold(`Options:`));
+    console.log(chalk.hex('#FF003C')(`  -h, --help`));
+    console.log(chalk.hex('#FF003C')(`      display help information`));
+    console.log(chalk.hex('#FF003C')(`  -t, --target`));
     console.log(
-      chalk.hex("#FF003C")(`      IPv4 address to get the information from`)
+      chalk.hex('#FF003C')(`      IPv4 address to get the information from`)
     );
-    console.log(chalk.hex("#FF003C")(`  -c, --count`));
+    console.log(chalk.hex('#FF003C')(`  -c, --count`));
     console.log(
-      chalk.hex("#FF003C")(
+      chalk.hex('#FF003C')(
         `      How many pages should the program search in Bing (default 100)`
       )
     );
-    console.log(chalk.hex("#FF003C")(`  -o, --output`));
+    console.log(chalk.hex('#FF003C')(`  -o, --output`));
     console.log(
-      chalk.hex("#FF003C")(`      Save the found websites in a json file`)
+      chalk.hex('#FF003C')(`      Save the found websites in a json file`)
     );
-    console.log(chalk.hex("#FF003C")(`  --tor`));
+    console.log(chalk.hex('#FF003C')(`  --tor`));
     console.log(
-      chalk.hex("#FF003C")(
+      chalk.hex('#FF003C')(
         `      Use tor as proxy and define the port socks tor port (default 9150)`
       )
     );
     console.log(
-      chalk.hex("#FF003C")(`  --proxy, --proxyType, --proxyHost, --proxyPort`)
+      chalk.hex('#FF003C')(`  --proxy, --proxyType, --proxyHost, --proxyPort`)
     );
     console.log(
-      chalk.hex("#FF003C")(
+      chalk.hex('#FF003C')(
         `      Use custom proxy, use the flags like this: --proxy --proxyType=SOCKS5 --proxyHost=IP --proxyPort=PORT, no auth is supported for the moment`
       )
     );
@@ -68,14 +68,14 @@ class ArgsHelper {
         target
       );
     if (!bValid) {
-      console.log(chalk.hex("#FF003C")(`Invalid IPv4 address`));
+      console.log(chalk.hex('#FF003C')(`Invalid IPv4 address`));
     }
     return bValid;
   }
-  tor(port) {
-    console.log(chalk.hex("#FF003C")('Tor flag enabled...'));
-    console.log(chalk.hex("#FF003C")('Checking tor connection'));
-    if (typeof port === "boolean") {
+  async tor(port) {
+    console.log(chalk.hex('#FF003C')('Tor flag enabled...'));
+    console.log(chalk.hex('#FF003C')('Checking tor connection'));
+    if (typeof port === 'boolean') {
       port = 9150;
     }
     const agent = new SocksProxyAgent(`socks://127.0.0.1:${port}`);
@@ -83,14 +83,29 @@ class ArgsHelper {
       httpAgent: agent,
       httpsAgent: agent,
     });
-    inst.get("https://check.torproject.org/api/ip").then((data) => {
+    try {
+      const data = await inst.get('https://check.torproject.org/api/ip');
       console.log(data);
-    }).catch((error) => {
-      console.log(chalk.hex("#FF003C")('It seems like tor is not running or maybe it is using another port?'));
-    })
+    } catch (error) {
+      console.log(
+        chalk.hex('#FF003C')(
+          'It seems like tor is not running or maybe it is using another port?'
+        )
+      );
+      return false;
+    }
   }
   proxy() {}
-  count() {}
+  count(number) {
+    try {
+      parseInt(number);
+    } catch (error) {
+      console.log(
+        chalk.hex('#FF003C')('Invalid number for count, please use an integer.')
+      );
+      return false;
+    }
+  }
 }
 
 /**
@@ -100,7 +115,7 @@ class ArgsHelper {
 
 // ToDo: beautify code too many returns in one function, reduce to one only
 
-module.exports = function argsValidation(args) {
+module.exports = async function argsValidation(args) {
   const argsHelper = new ArgsHelper();
   const allArgs = {
     help: argsHelper.help,
@@ -119,7 +134,7 @@ module.exports = function argsValidation(args) {
     result.stop = true;
   } else {
     for (let prop in args) {
-      if (prop === "help" || prop === "h") {
+      if (prop === 'help' || prop === 'h') {
         argsHelper.help();
         result.stop = true;
         break;
@@ -129,7 +144,11 @@ module.exports = function argsValidation(args) {
         result.stop = true;
         break;
       }
-      result.stop = !allArgs[prop](args[prop]);
+      if (prop === 'tor') {
+        result.stop = await !allArgs[prop](args[prop]);
+      } else {
+        result.stop = !allArgs[prop](args[prop]);
+      }
       if (result.stop) break;
     }
   }
